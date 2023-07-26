91ad48e0f7aba145cff56242593150da91e72412
gajjar-mohit <d22ce163@charusat.edu.in>
2023-07-26 10:29:06 +0530

    Textfield

--- test/lib/main.dart
+++ test/lib/main.dart
@@ -23,26 +23,38 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  String name = "";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Hello world"),
       ),
-      body: const Center(
+      body: Container(
+        margin: const EdgeInsets.symmetric(horizontal: 20),
         child: Column(
           children: [
-            Text(
-              "Text 1",
-              style: TextStyle(fontSize: 30),
+            const SizedBox(
+              height: 20,
+            ),
+            TextField(
+              onChanged: (val) {
+                setState(() {
+                  name = val;
+                });
+              },
+              decoration: const InputDecoration(
+                  hintText: "mohit",
+                  label: Text("Enter name"),
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(18)))),
+            ),
+            const SizedBox(
+              height: 10,
             ),
             Text(
-              "Text 1",
-              style: TextStyle(fontSize: 30),
-            ),
-            Text(
-              "Text 1",
-              style: TextStyle(fontSize: 30),
+              name,
+              style: const TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
             ),
           ],
         ),
